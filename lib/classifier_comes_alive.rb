9cb5132d1ed96f3a25c05a7d41ce4f745a544ee3
Ryan Oblak <rroblak@gmail.com>
2013-04-17 17:43:29 -0700

    moved lsi

--- lib/classifier_comes_alive.rb
+++ lib/classifier_comes_alive.rb
@@ -2,4 +2,5 @@ require "classifier_comes_alive/version"
 
 module ClassifierComesAlive
   autoload :Bayes, 'classifier_comes_alive/bayes'
+  autoload :ClassifierComesAlive, 'classifier_comes_alive/lsi'
 end
