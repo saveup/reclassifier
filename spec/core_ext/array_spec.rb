b4825a56415c1e50d254c88aafc8df725c77022f
Ryan Oblak <rroblak@gmail.com>
2013-04-22 16:00:11 -0700

    converted array test to spec

---
+++ spec/core_ext/array_spec.rb
@@ -0,0 +1,13 @@
+require File.join(Dir.pwd, 'spec', 'spec_helper.rb')
+
+describe Array do
+  describe "sum_with_identity" do
+    it "should sum the array" do
+      [1,2,3].sum_with_identity.should eq(6)
+    end
+
+    it "should return 0 when it encounters an empty array" do
+      [].sum_with_identity.should eq(0)
+    end
+  end
+end
