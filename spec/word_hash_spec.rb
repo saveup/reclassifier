2f56dd635b1de3760d40145726cbbfa47f8cd617
Ryan Oblak <rroblak@gmail.com>
2013-04-23 16:20:08 -0700

    when cleaning convert punctuation to spaces

--- spec/word_hash_spec.rb
+++ spec/word_hash_spec.rb
@@ -15,5 +15,15 @@ describe Reclassifier::Bayes do
 
   	  subject.clean_word_hash("here are some good words of test's. I hope you love them!").should eq(hash)
     end
+
+    it 'should convert non-word characters to spaces' do
+      subject.clean_word_hash('Payment-Transfer').should eq(:payment => 1, :transfer => 1)
+    end
+  end
+
+  [:word_hash, :clean_word_hash].each do |method|
+    it "#{method} should trim each word" do
+      subject.send(method, "test    test123   \t\t\t aaa").should eq(:test => 1, :test123 => 1, :aaa => 1)
+    end
   end
 end
